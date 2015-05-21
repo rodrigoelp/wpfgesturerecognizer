ca1bc551984b64687c336b01bd620d68d3eca9f3
Rodrigo Landaeta <rperera@cochlear.com>
2015-05-21 14:55:48 +1000

    Changed GestureRecognizer to include:   - AutoManipulationEnabled setting up FrameworkElement.IsManipulationEnabled     property by default on the associated object.   - Filter gestures based on TriggerOnGesture property.   - And use the absolute distance rather than the delta to discriminate between       gestures.
    Lowered the framework requirement.

--- src/Org.Interactivity.Recognizer/GestureRecognizer.cs
+++ src/Org.Interactivity.Recognizer/GestureRecognizer.cs
@@ -5,29 +5,58 @@ using System.Windows.Interactivity;
 
 namespace Org.Interactivity.Recognizer
 {
-    public class GestureRecognizer : TriggerBase<FrameworkElement>
+    public sealed class GestureRecognizer : TriggerBase<FrameworkElement>
     {
+        //This magic number corresponds to observations of what makes a tap versus a swipe or flick.
         private const int TapThreshold = 40;
 
-        public Gesture TriggerOnGesture { get; set; }
+        public static readonly DependencyProperty AutoManipulationEnabledProperty = DependencyProperty.Register(
+            "AutoManipulationEnabled", typeof(bool), typeof(GestureRecognizer), new PropertyMetadata(true, HandleAutoManipulationEnabled));
 
-        public GestureRecognizer()
+        public static readonly DependencyProperty TriggerOnGestureProperty = DependencyProperty.Register(
+            "TriggerOnGesture", typeof(Gesture), typeof(GestureRecognizer), new PropertyMetadata(Gesture.All));
+
+        public bool AutoManipulationEnabled
         {
-            TriggerOnGesture = Gesture.All;
+            get { return (bool)GetValue(AutoManipulationEnabledProperty); }
+            set { SetValue(AutoManipulationEnabledProperty, value); }
+        }
+
+        public Gesture TriggerOnGesture
+        {
+            get { return (Gesture)GetValue(TriggerOnGestureProperty); }
+            set { SetValue(TriggerOnGestureProperty, value); }
         }
 
         protected override void OnAttached()
         {
             base.OnAttached();
-            AssociatedObject.ManipulationStarting += HandleManipulationStarting;
-            AssociatedObject.ManipulationCompleted += HandleManipulationCompleted;
+            if (AutoManipulationEnabled)
+            {
+                AssociatedObject.IsManipulationEnabled = AutoManipulationEnabled;
+            }
+
+            AssociatedObject.Loaded += (sender, args) =>
+            {
+                AssociatedObject.ManipulationStarting += HandleManipulationStarting;
+                AssociatedObject.ManipulationCompleted += HandleManipulationCompleted;
+            };
         }
 
         protected override void OnDetaching()
         {
+            base.OnDetaching();
             AssociatedObject.ManipulationStarting -= HandleManipulationStarting;
             AssociatedObject.ManipulationCompleted -= HandleManipulationCompleted;
-            base.OnDetaching();
+        }
+
+        private static void HandleAutoManipulationEnabled(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var instance = d as GestureRecognizer;
+            if (instance != null && instance.AssociatedObject != null)
+            {
+                instance.AssociatedObject.IsManipulationEnabled = instance.AutoManipulationEnabled;
+            }
         }
 
         private void HandleManipulationStarting(object sender, ManipulationStartingEventArgs e)
@@ -38,7 +67,11 @@ namespace Org.Interactivity.Recognizer
 
         private void HandleManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
         {
-                InvokeActions(ToSwipeGesture(e.TotalManipulation.Translation));
+            var gesture = ToSwipeGesture(e.TotalManipulation.Translation);
+            if (TriggerOnGesture == Gesture.All || TriggerOnGesture == gesture)
+            {
+                InvokeActions(gesture);
+            }
         }
 
         private static Gesture ToSwipeGesture(Vector translation)
@@ -47,7 +80,7 @@ namespace Org.Interactivity.Recognizer
             var deltaY = translation.Y;
             var distX = Math.Abs(deltaX);
             var distY = Math.Abs(deltaY);
-            if (deltaX <= TapThreshold && deltaY <= TapThreshold)
+            if (distX <= TapThreshold && distY <= TapThreshold)
             {
                 return Gesture.Tap;
             }
