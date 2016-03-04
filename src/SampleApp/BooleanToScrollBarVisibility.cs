14ef3bf8a08f68e1b2ad04a0faa0445b8d460d8a
Eric Rodewald <eric.rode@gmail.com>
2016-03-04 10:57:39 -0600

    Add new converters for use in SampleApp demo.

---
+++ src/SampleApp/BooleanToScrollBarVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SampleApp
+{
+    public class BooleanToScrollBarVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var enabled = bool.Parse(value.ToString());
+            return enabled ? ScrollBarVisibility.Visible : ScrollBarVisibility.Disabled;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var visibility = (ScrollBarVisibility) value;
+            return visibility == ScrollBarVisibility.Visible;
+        }
+    }
+}
---
+++ src/SampleApp/HalfLengthConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SampleApp
+{
+    public class HalfLengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return (double) value/2.0d;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
